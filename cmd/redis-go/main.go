c607ed13afa256d9036dfc9bb355fc1559e5cf9b
strCarne <semionvoevoda@gmail.com>
2024-10-03 12:45:09 +0300

    feat: handle PING

--- cmd/redis-go/main.go
+++ cmd/redis-go/main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		return
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println("Error accepting connection: ", err.Error())
+		}
+
+		buf := make([]byte, 216)
+		bytesRead, err := conn.Read(buf)
+		if err != nil {
+			log.Println("Error reading: ", err.Error())
+			continue
+		}
+
+		received := string(buf[:bytesRead])
+		log.Println(received)
+
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
